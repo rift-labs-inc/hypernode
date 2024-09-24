280d29546ff447f28a4fd0212e3093c253111aba
Cliff Syner <c4syner@gmail.com>
2024-09-23 22:17:58 -0500

    update contracts

--- populate_artifacts.sh
+++ populate_artifacts.sh
@@ -4,4 +4,4 @@ rm -rf artifacts/*
 mv contracts/out/RiftExchange.sol/RiftExchange.json artifacts/
 mv contracts/out/WETH.sol/WETH.json artifacts/
 mv contracts/out/RiftExchange.sol/IERC20.json artifacts/ 
-mv contracts/out/ERC20.sol/ERC20.json artifacts/
+mv contracts/out/MockUSDT.sol/MockUSDT.json artifacts/
