44468bf520d535141597b5da701c6c26b863902c
Cliff Syner <c4syner@gmail.com>
2024-09-30 16:25:46 -0700

    fix: sync contracts

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 # Helper script to compile contract artifacts and move them to artifacts/ dir 
-git submodule update --init --recursive --remote
+git submodule update --init --remote
 cd contracts/ && forge compile && cd ..
 rm -rf artifacts/*
 mv contracts/out/RiftExchange.sol/RiftExchange.json artifacts/
