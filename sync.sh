23e9c9fa351b4305e0263cc25a834c69f2ee19cf
Cliff Syner <c4syner@gmail.com>
2024-10-04 01:04:45 -0700

    sync contracts

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 # Helper script to compile contract artifacts and move them to artifacts/ dir 
 git submodule update --init --remote
-cd contracts/ && forge compile && cd ..
+cd contracts/ && forge compile --via-ir && cd ..
 rm -rf artifacts/*
 mv contracts/out/RiftExchange.sol/RiftExchange.json artifacts/
 mv contracts/out/RiftExchange.sol/IERC20.json artifacts/ 
-mv contracts/out/tests/MockUSDT.sol/MockUSDT.json artifacts/
+mv contracts/out/MockUSDT.sol/MockUSDT.json artifacts/
